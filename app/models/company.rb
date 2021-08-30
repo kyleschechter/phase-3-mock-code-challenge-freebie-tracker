4797d55768105834e42eab8f9bb68c934f0fb857
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-08-30 10:28:06 -0400

    done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,17 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev, item, value)
+    Freebie.create(
+      item_name: item,
+      value: value, 
+      company_id: self.id, 
+      dev_id: dev.id
+      )
+  end
+
+  def self.oldest_company
+    self.all.order(:founding_year)[0]
+  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item)
+    self.freebies.any? {|free| free.item_name == item}
+  end
+
+  def give_away(dev, freebie)
+    freebie.dev == self ? freebie.update(dev: dev) : puts "That's not yours to give away pal"
+  end
 end
